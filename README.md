18dc4cbc8f38633ca70585f7973bd310cfa3f92a
Reuben Honigwachs <reuben@honigwachs.de>
2020-04-01 09:58:38 +0200

    add further explanations

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Dirty scripts to upstream project [luftdaten.info](https://luftdaten.info/)
+Dirty scripts to upstream project [luftdaten.info](https://luftdaten.info/).
+Based on the question whether the sensor readings for a country during a certain
+period can be visualized. 
 
 Get sensor IDs by country with `./get-sensor-ids.sh -c za`
 
--- get-sensor-ids.sh
+++ get-sensor-ids.sh
@@ -11,9 +11,12 @@ do
 case "${option}"
 in
 c) COUNTRY=${OPTARG^^};;
+# TODO currently fixed to PM sensor SDS011, but the project supports more
 t) TYPE=${OPTARG^^};;
+# TODO output file
 f) FILE=${OPTARG};;
 esac
 done
 
+# API doc at https://github.com/opendata-stuttgart/meta/wiki/EN-APIs
 curl https://data.sensor.community/airrohr/v1/filter/country={$COUNTRY} | jq -c '.[].sensor | select (.sensor_type.name == "SDS011") | .id '
